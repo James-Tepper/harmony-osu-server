18acc27d49a490ccc25e5c9aaab6c907fa8775d7
James Tepper <jamesatepper@gmail.com>
2023-06-26 16:28:10 -0400

    login_reply almost done | needs review

--- app/repositories/stats.py
+++ app/repositories/stats.py
@@ -8,72 +8,72 @@ from app.database import database
 READ_PARAMS = """\
         user_id,
         action,
-        status_text,
-        beatmap_checksum,
-        current_mods,
-        play_mode,
+        info_text,
+        beatmap_md5,
+        mods,
+        mode,
         beatmap_id,
         ranked_score,
         accuracy,
         play_count,
         total_score,
-        rank,
-        performance
+        global_rank,
+        performance_points
 """
 
 
 class Stats(TypedDict):
     user_id: int
     action: int
-    status_text: str
-    beatmap_checksum: str
-    current_mods: int
-    play_mode: int
+    info_text: str
+    beatmap_md5: str
+    mods: int
+    mode: int
     beatmap_id: int
     ranked_score: int
     accuracy: float
     play_count: int
     total_score: int
-    rank: int
-    performance: int
+    global_rank: int
+    performance_points: int
 
 
 async def create(
     user_id: int,
     action: int,
-    status_text: str,
-    beatmap_checksum: str,
-    current_mods: int,
-    play_mode: int,
+    info_text: str,
+    beatmap_md5: str,
+    mods: int,
+    mode: int,
     beatmap_id: int,
     ranked_score: int,
     accuracy: float,
     play_count: int,
     total_score: int,
-    rank: int,
-    performance: int,
+    global_rank: int,
+    performance_points: int,
 ) -> Stats:
     stats = await database.fetch_one(
         query=f"""
             INSERT INTO stats
-            (user_id, action, status_text, beatmap_checksum, current_mods, play_mode, beatmap_id, ranked_score, accuracy, play_count, total_score, rank, performance)
-            VALUES (:user_id, :action, :status_text, :beatmap_checksum, :current_mods, :play_mode, :beatmap_id, :ranked_score, :accuracy, :play_count, :total_score, :rank, :performance)
+            (user_id, action, info_text, beatmap_md5, mods, mode, beatmap_id, ranked_score, accuracy, play_count, total_score, global_rank, performance_points)
+            VALUES (:user_id, :action, :info_text, :beatmap_md5, :mods, :mode, :beatmap_id, :ranked_score, :accuracy, :play_count, :total_score, :global_rank, :performance_points)
             RETURNING {READ_PARAMS}
         """,
         values={
             "user_id": user_id,
             "action": action,
-            "status_text": status_text,
-            "beatmap_checksum": beatmap_checksum,
-            "current_mods": current_mods,
-            "play_mode": play_mode,
+            "info_text": info_text,
+            "beatmap_md5": beatmap_md5,
+            "mods": mods,
+            "mode": mode,
             "beatmap_id": beatmap_id,
             "ranked_score": ranked_score,
             "accuracy": accuracy,
             "play_count": play_count,
             "total_score": total_score,
-            "rank": rank,
-            "performance": performance,
+            "global_rank": global_rank,
+            "performance_points": performance_points,
         },
     )
 
